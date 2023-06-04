dbdaf33b942a06fdc93848930adffc583935b372
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-06-04 14:37:21 -0700

    Added all prompts, need to fix prompt JS

--- index.js
+++ index.js
@@ -129,10 +129,6 @@ const textNodes = [
     ]
   },
   {
-    id: ,
-    text: 
-  },
-  {
     id: 9,
     text: "You sneak past the wizard and there are two paths in front of you. There is a room with a strange glow and an opening to the outside.",
     options: [
@@ -255,25 +251,212 @@ const textNodes = [
         nextText: 26,
       }
     ]
+  },
+  {
+    id: 25,
+    text: "You lunge at the troll, but he flicks his hand and you freeze in place. He shakes his head at your poor decision making. He turns you into a little mouse.",
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 26,
+    text: "You are scolded by the troll for being selfish. You leave empty-handed. As you drive home, you reflect on your behavior. You change for the better after your revelation. Months later, you hear news of Sarah opening a charity in town.",
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 22,
+    text: "You and Sarah exchange stories for a few hours, then you say your goodbyes. You spend the next weeks searching the farm alone. You try to recall the description of the path from Sarah's story, but you struggle and never find the treasure.",
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 23,
+    text:  "Sarah runs over. A troll appears behind you, admiring your companionship and willingness to share your wealth. The troll waves his hand, opening the treasure chest. You see it filled to the top with solid gold coins. You hug each other in celebration. You split the gold coins evenly, roughly giving you $2 million each.",
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 6,
+    text: "He leads you to a little box on the ground. You hold him back, afraid of what the object might be. Upon further investigation, you discover it's a tiny house! You look closer and see a tiny flicker of light on the inside. Spike tries to push past you to get to the house. Do you let him approach it?",
+    options: [
+      {
+        text: "Let him",
+        nextText: 27,
+      },
+      {
+        text: "Call Spike to come back",
+        nextText: 28,
+      }
+    ]
+  },
+  {
+    id: 27,
+    text: `Spike runs towards the house and sniffs aggressively, shaking the tiny house. You see the door open and a tiny fairy screams! It waves it's hand, releasing a puff of glittery dust into Spike's face. The door closes and Spike is dazed, but after a few moments, remains unharmed. Do you interrogate the fairy or avoid conflict?`,
+    options: [
+      {
+        text: "Interrogate the fairy",
+        nextText: 28,
+      },
+      {
+        text: "Avoid conflict",
+        nextText: 29,
+      }
+    ]
+  },
+  {
+    id: 28,
+    text: "The fairy opens the door, apologizing. It was startled and accidentally charmed Spike. The fairy, named Fawn, gives Spike an antidote to reverse the charm. Fawn is sweet and caring to animals. You become friends and you offer to take Fawn home to live with you. Fawn accepts. You, Spike, and Fawn go home to live together.",
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 29,
+    text: `During the walk home, Spike starts to cough. You grow concerned as the coughing lasts a few minutes. You ask, "Are you okay?" You hear a voice respond, "Yes, I'm fine." Spike spoke! The fairy seems to have charmed Spike into being able to speak. You take him home and have interesting conversations.`,
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 28,
+    text: "You continue to hold him back. The tiny door opens, revealing a little fairy! She cautiously flies over to you. Her name is Fawn. She tells you about her fairy friends and a leprechaun who also lives at the lake.",
+    options: [
+      {
+        text: "Meet the fairies",
+        nextText: 30,
+      },
+      {
+        text: "Meet the leprechaun",
+        nextText: 31,
+      }
+    ]
+  },
+  {
+    id: 30,
+    text: "Fawn flies away and comes back with her friends. They make both of you flower crowns and gently place them on your heads. They fly around you, giggling and playing with Spike. You and Spike leave for the day, but always make time to see your new fairy friends.",
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 31,
+    text: "Fawn takes you down to the leprechaun's home. She introduces you, and he welcomes you with open arms. You are amazed with his interior design skills, and he offers to decorate your house. You take him up on his offer and he leaves you with a beautiful cottagecore home.",
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 7,
+    text: "Spike comes running back to you. You continue walking and find a small home. You knock on the door, and a leprechaun opens it! He invites you inside. His name is Albie, he just finished baking an apple pie. He shares a slice with you and he gives you a tour of his home. You see a room filled with a golden statues and gold coins.",
+    options: [
+      {
+        text: "Ask about the gold statues",
+        nextText: 32,
+      },
+      {
+        text: "Ask about the gold coins",
+        nextText: 33,
+      }
+    ]
+  },
+  {
+    id: 32,
+    text: "Albie shows you his collection of awards he won during the Leprechaun Olympics. He is a professional pickleball player. He offers to be your pickleball mentor. Do you accept?",
+    options: [
+      {
+        text: "Learn pickleball",
+        nextText: 34,
+      },
+      {
+        text: "Refuse",
+        nextText: 35,
+      }
+    ]
+  },
+  {
+    id: 34,
+    text: "You and Albie practice pickleball for months to compete in a pickleball tournament. You win numerous awards, thanks to your mentor.",
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 35,
+    text: `"No worries, it's not for everyone" he says. He tells you stories of his competitions and he gives you the recipe for his amazing apple pie.`,
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 33,
+    text: "Albie tells you that after every rainfall, the leprechauns have a gold hunt. They race to the end of the rainbow and the winner gets the gold. There are no rules against having a human compete. He asks if you would like to join during the next hunt, but warns that the other leprechauns are competitive and violent.",
+    options: [
+      {
+        text: "Join the gold hunt",
+        nextText: 36,
+      },
+      {
+        text: "Pass",
+        nextText: 37,
+      }
+    ]
+  },
+  {
+    id: 36,
+    text: "You wait for the next rainy day and join Albie in the gold hunt. Due to your human size, you carry Albie and move faster than the other teams. You beat everyone to the end of the rainbow and win the gold prize! Albie gives you the entire pot of gold, because he has plenty of his own.",
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
+  },
+  {
+    id: 37,
+    text: `Albie tells you that he is a frequent winner of the gold hunt, and has plenty of gold already. He fills up a small pouch with a handful of gold coins and hands it to you. "Here, you need it more than I do."`,
+    options: [
+      {
+        text: "Restart",
+        nextText: 1
+      }
+    ]
   }
 ]
 
 // Begin!
 startGame()
-
-/* Blank template to copy and paste
-  {
-    id: ,
-    text: 
-    options: [
-      {
-        text: 
-        nextText: ,
-      },
-      {
-        text: 
-        nextText: ,
-      }
-    ]
-  }
-*/
